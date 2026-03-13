76560c17d6b067d9eb15079018024bac7cdbc54f
park <parkch2463@gmail.com>
2026-03-13 15:42:23 +0900

    feat: 필터 기반 서비스 페이지 ui 및 기능 구현(#26)
    - 필터 서비스 구현

---
+++ src/constants/bookCategories.ts
@@ -0,0 +1,15 @@
+export const CATEGORIES = [
+  {label: '전체', id: 0},
+  { label: '소설', id: 1 },
+  { label: '에세이', id: 51371 },
+  { label: '자기계발', id: 70216 },
+  { label: '경제', id: 174 },
+  { label: '과학', id: 51002 },
+]
+
+export const SORT_OPTIONS = [
+  { label: '베스트셀러', value: 'Bestseller' },
+  { label: '신상품', value: 'ItemNewAll' },
+  { label: '평점순', value: 'CustomerRating' },
+  { label: '출시년도', value: 'PublishYear' },
+];
